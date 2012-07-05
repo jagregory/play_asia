832e9c319699e1fb9e9b33d4dd1169286323bf5d
James Gregory <james@jagregory.com>
2012-07-06 00:24:43 +1000

    Added friendly mask parameter support

--- lib/play_asia/api_listing.rb
+++ lib/play_asia/api_listing.rb
@@ -1,15 +1,40 @@
 class PlayAsia::API
   def listing(opts = {})
-    response = query(opts.merge({ query: :listing }))
+    opts = process_friendly_keys opts.merge({ query: :listing })
+    response = query opts
     
     raise "Query error: '#{response.error_message}'" if response.error?
     
     response.content.search('item').map do |xml|
       item = {}
       xml.search('*').each do |node|
-        item[node.name] = node.text
+        item[node.name.to_sym] = node.text
       end
       item
     end
+  end
+  
+  private
+  MASK = {
+    price: 'p',
+    barcodes: 'j',
+    manufacturer_codes: 'm',
+    sale_info: 's',
+    release_date: 'r',
+    name: 'n',
+    image: 'i',
+    url: 'l',
+    affiliate_url: 'a',
+    genre: 'g',
+    version: 'v',
+    encoding: 'e',
+    compatibility: 'c'
+  }
+  
+  def process_friendly_keys opts
+    if opts[:mask] && opts[:mask].is_a?(Array)
+      opts[:mask] = opts[:mask].map { |k| MASK[k] || k }.join
+    end
+    opts
   end
 end
